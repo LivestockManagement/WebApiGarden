f439a805286eb3e79aa19f47a1baa4c06bad79f1
Kahne Raja <kahneraja@gmail.com>
2014-03-28 14:35:23 +1100

    Initial setup.

---
+++ WebApiGarden.Web.Api/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiGarden.Web.Api.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        { 
+            return new string[] { "Values 1", "Values 2"};
+        }
+    }
+}
